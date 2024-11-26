261e16e4b92ed3154ea05b766ecd177bf0d06537
Nathan Shauer <nathan.sh@gmail.com>
2024-11-26 13:22:01 -0300

    feat: added team and smooth scrolling

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     setInterval(showNextPhoto, 3000); // Change photo every 5 seconds
+
+    // Add smooth scrolling to all internal links with class "nav-link"
+    document.querySelectorAll('a.nav-link').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            // Check if the link is an internal link
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                e.preventDefault();
+
+                // Store hash
+                const hash = this.hash;
+
+                // Using scrollIntoView to add smooth page scroll
+                document.querySelector(hash).scrollIntoView({
+                    behavior: 'smooth'
+                });
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.history.pushState(null, null, hash);
+            }
+        });
+    });
 });
